05ceeeb0a867a4e30bcc7285daef0fcf8046081c
Andrew Mirshafiee <andrewmirs@csu.fullerton.edu>
2019-01-18 14:15:17 -0800

    cart update if item is deleted from inventory

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -3,8 +3,9 @@ import { Mutation } from 'react-apollo';
 import { TOGGLE_CART_MUTATION } from './Cart';
 import Link from 'next/link';
 import NavStyles from './styles/NavStyles';
-import User from './User';
+import CartCount from './CartCount';
 import Signout from './Signout';
+import User from './User';
 
 const Nav = () => (
     <User>
@@ -27,7 +28,10 @@ const Nav = () => (
                         <Signout />
                         <Mutation mutation={TOGGLE_CART_MUTATION}>
                             {(toggleCart) => (
-                                <button onClick={toggleCart}>My Cart</button>
+                                <button onClick={toggleCart}>
+                                    My Cart
+                                    <CartCount count={me.cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0)} />
+                                </button>
                             )}
                         </Mutation>
                     </Fragment> 
