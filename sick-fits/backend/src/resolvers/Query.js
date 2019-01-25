99db573bf658f97ba0a83f71cbbaa8f1274c4fe8
Andrew Mirshafiee <andrewmirs@csu.fullerton.edu>
2019-01-25 14:49:42 -0800

    single orders page renders with all the necessary info; update the users overall orders page next

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -8,26 +8,48 @@ const Query = {
 
     me( parent, args, ctx, info ){
         // Check if there is a current userId
-        if(!ctx.request.userId){
-            return null;
-        }
-        return ctx.db.query.user({
-            where: { id: ctx.request.userId }
-        }, info);
+            if(!ctx.request.userId){
+                return null;
+            }
+            return ctx.db.query.user({
+                where: { id: ctx.request.userId }
+            }, info);
     },
 
     async users( parent, args, ctx, info ) {
         // 1. Check if they are logged in
-        if (!ctx.request.userId) {
-            throw new Error('You must be logged in!');
-        }
+            if (!ctx.request.userId) {
+                throw new Error('You must be logged in!');
+            }
 
         // 2. Check if the user has the permissions to query all the users
-        hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
+            hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
 
         // 3. If they do, query all the users
-        return ctx.db.query.users({}, info);
-    },
+            return ctx.db.query.users({}, info);
+        },
+
+    async order( parent, args, ctx, info ) {
+        // 1. Make sure they are logged in
+            if (!ctx.request.userId) {
+                throw new Error('You are not logged in!');
+            }
+
+        // 2. Query the current order
+            const order = await ctx.db.query.order({
+                where: { id: args.id },
+            }, info);
+
+        // 3. Check if they have the permissions to see this order
+            const ownsOrder = order.user.id === ctx.request.userId;
+            const hasPermissionToSeeOrder = ctx.request.user.permissions.includes('ADMIN');
+            if (!ownsOrder || !hasPermissionToSeeOrder) {
+                throw new Error("You're not authorized to see this at this time.")
+            }
+
+        // 4. Return the order
+            return order;
+    }
 
 };
 
