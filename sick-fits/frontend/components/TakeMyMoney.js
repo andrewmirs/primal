709d40286c697687a6db80bb20518bcb97a074e4
Andrew Mirshafiee <andrewmirs@csu.fullerton.edu>
2019-01-24 22:46:05 -0800

    updated data model to include orderItems and the order; started mutation for creating the order; saving orders to the database next!

--- sick-fits/frontend/components/TakeMyMoney.js
+++ sick-fits/frontend/components/TakeMyMoney.js
@@ -9,33 +9,62 @@ import calcTotalPrice from '../lib/calcTotalPrice';
 import Error from './ErrorMessage';
 import User, { CURRENT_USER_QUERY } from './User';
 
+const CREATE_ORDER_MUTATION = gql `
+    mutation createOrder($token: String!) {
+        createOrder(token: $token) {
+            id
+            charge
+            total
+            items {
+                id
+                title
+            }
+        }
+    }
+`;
+
 function totalItems(cart) {
     return cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0);
 }
 
 class TakeMyMoney extends Component {
     
-    onToken = (resp) => {
+    onToken = ( resp, createOrder ) => {
         console.log('On Token Called!!');
         console.log(resp.id);
+        // Manually call the mutation once we have the stripe token
+        createOrder({
+            variables: {
+                token: resp.id
+            }
+        }).catch( err => {
+            alert(err.message);
+        });
     }
     
     render(){
         return (
             <User>
                 {({ data: { me } }) => (
-                    <StripeCheckout
-                        amount={calcTotalPrice(me.cart)}
-                        name="Sick Fits"
-                        description={`Order of ${totalItems(me.cart)} items`}
-                        image={me.cart[0].item && me.cart[0].item.image}
-                        stripeKey="pk_test_JqFUKdctmgzJN9XjMMkt6NKK"
-                        currency="USD"
-                        email={me.email}
-                        token={resp => this.onToken(resp)}
+                    <Mutation 
+                        mutation={CREATE_ORDER_MUTATION}
+                        refetchQueries={[{ query: CURRENT_USER_QUERY }]}
                     >
-                        {this.props.children}
-                    </StripeCheckout>
+                        {(createOrder) => (
+                            <StripeCheckout
+                                amount={calcTotalPrice(me.cart)}
+                                name="Sick Fits"
+                                description={`Order of ${totalItems(me.cart)} items`}
+                                image={me.cart[0].item && me.cart[0].item.image}
+                                stripeKey="pk_test_JqFUKdctmgzJN9XjMMkt6NKK"
+                                currency="USD"
+                                email={me.email}
+                                token={resp => this.onToken(resp, createOrder)}
+                            >
+                                {this.props.children}
+                            </StripeCheckout>
+                        )}
+                    </Mutation>
                 )}
             </User>
         );
